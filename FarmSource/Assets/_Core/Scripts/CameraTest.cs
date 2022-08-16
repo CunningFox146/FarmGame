ce5ef8e905c45806e9cde61deb0f98f22362f1ea
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-08-16 19:31:50 +0300

    Added camera test

---
+++ FarmSource/Assets/_Core/Scripts/CameraTest.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Farm
+{
+    public class CameraTest : MonoBehaviour
+    {
+        private Tween _tween;
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                _tween?.Complete();
+                _tween = null;
+
+                _tween = transform.DORotate(Vector3.up * 30f, 0.5f).SetRelative(true);
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                _tween?.Complete();
+                _tween = null;
+
+                _tween = transform.DORotate(Vector3.up * -30f, 0.5f).SetRelative(true);
+            }
+        }
+    }
+}
