470385dc6f8f5cac5855e29fda94a1ec18bbe78f
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-08-19 14:15:38 +0300

    Added collect anim

--- FarmSource/Assets/_Core/Scripts/Animations/PlayerAnimations.cs
+++ FarmSource/Assets/_Core/Scripts/Animations/PlayerAnimations.cs
@@ -4,7 +4,8 @@ namespace Farm.Animations
 {
     public static class PlayerAnimations
     {
-        public static readonly int Idle = Animator.StringToHash("Idle");
-        public static readonly int Run = Animator.StringToHash("Run");
+        public static readonly int Idle = Animator.StringToHash(nameof(Idle));
+        public static readonly int Run = Animator.StringToHash(nameof(Run));
+        public static readonly int Collect = Animator.StringToHash(nameof(Collect));
     }
 }
