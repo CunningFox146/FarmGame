26447f293c303274f03e15f6837086726bd7602b
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-08-26 14:06:08 +0300

    Small fixes

---
+++ FarmSource/Assets/_Core/Scripts/Animations/FaceAppliers/TwoFaceApplier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Farm.Animations.FaceAppliers
+{
+    public class TwoFaceApplier : IFaceApplier
+    {
+        private SpriteRenderer _spriteRenderer;
+        private Transform _transform;
+
+        public TwoFaceApplier(SpriteRenderer spriteRenderer)
+        {
+            _spriteRenderer = spriteRenderer;
+            _transform = spriteRenderer.transform;
+        }
+
+        public void ApplyFace()
+        {
+            _spriteRenderer.flipX = Mathf.Abs(_transform.eulerAngles.y) > 180;
+        }
+    }
+}
