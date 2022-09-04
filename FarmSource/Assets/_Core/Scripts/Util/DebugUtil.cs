036ae6ae24079730fe28e30623fb061d9825347a
Makar Dzezhamesau <dzezhamesau.work@gmail.com>
2022-09-04 12:55:00 +0300

    Cleaned up
    Changed shadows to be static and dynamic

---
+++ FarmSource/Assets/_Core/Scripts/Util/DebugUtil.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Farm.Util
+{
+    public static class DebugUtil
+    {
+        public static void DrawEllipse(Vector3 pos, Vector3 forward, Vector3 up, float radiusX, float radiusY, int segments, Color color, float duration = 0)
+        {
+            float angle = 0f;
+            Quaternion rot = Quaternion.LookRotation(forward, up);
+            Vector3 lastPoint = Vector3.zero;
+            Vector3 thisPoint = Vector3.zero;
+
+            for (int i = 0; i < segments + 1; i++)
+            {
+                thisPoint.x = Mathf.Sin(Mathf.Deg2Rad * angle) * radiusX;
+                thisPoint.y = Mathf.Cos(Mathf.Deg2Rad * angle) * radiusY;
+
+                if (i > 0)
+                {
+                    Debug.DrawLine(rot * lastPoint + pos, rot * thisPoint + pos, color, duration);
+                }
+
+                lastPoint = thisPoint;
+                angle += 360f / segments;
+            }
+        }
+    }
+}
